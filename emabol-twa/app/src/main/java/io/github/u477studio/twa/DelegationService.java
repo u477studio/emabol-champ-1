bd936964201702f424e55822b4ed481fbe5d9ee7
emarufino <70921692+emarufino@users.noreply.github.com>
2026-03-22 12:15:46 -0300

    upgrades

---
+++ emabol-twa/app/src/main/java/io/github/u477studio/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package io.github.u477studio.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
